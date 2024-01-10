40e60f18f28f8ae0f8d2082e5cfd57cfe56b4b11
Crystal Tran <c.t.tran95@gmail.com>
2024-01-09 20:58:23 -0500

    started making fetchMock test

--- shipItApi.test.js
+++ shipItApi.test.js
@@ -1,17 +1,37 @@
 "use strict";
 
-const {
+const fetchMock = require("fetch-mock");
+
+let {
   shipProduct,
+  SHIPIT_SHIP_URL
 } = require("./shipItApi");
+
+// shipProduct = jest.fn();
 
 
 test("shipProduct", async function () {
-  const shipId = await shipProduct({
-    productId: 1000,
-    name: "Test Tester",
-    addr: "100 Test St",
-    zip: "12345-6789",
+  fetchMock.post(SHIPIT_SHIP_URL, {
+    body: {
+      itemId: 1000,
+      name: "Test",
+      addr: "123 Test",
+      zip: "1234",
+      shipId: 700
+     },
+    status: 200
   });
 
-  expect(shipId).toEqual(expect.any(Number));
+  const order = {
+    productId: 1000,
+    name: "Test",
+    addr: "123 Test",
+    zip: "1234"
+  }
+
+  const res = await shipProduct(order);
+
+  console.log('res:', res);
+
+  expect(res.body.receipt.shipId).toEqual(700);
 });
